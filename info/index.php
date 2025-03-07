496311b8928d7f7214e6f088c425bf6dc4df7134
g s <gerardoks2004@gmail.com>
2025-03-07 16:59:47 -0700

    strategy pattern implementation

--- info/index.php
+++ info/index.php
@@ -1,13 +1,39 @@
 <?php
-
-require_once 'Board.php';
-require_once 'MoveStrategy.php';
-require_once 'RandomStrategy.php';
+session_start();
+require_once '../Board.php';
+require_once '../Strategy/MoveStrategy.php';
+require_once '../Strategy/RandomStrategy.php';
+require_once '../Strategy/SmartStrategy.php';
 
 $gameInfo = array( "width" => 7, "height" => 6, "strategies" => array("Smart", "Random"));
 
-header('Content-Type: application/json');
+
+
+
+if (!isset($_SESSION['strategy'])):
+?>
+<html>
+    <form method="POST">
+        <label for="strategy">Choose a Strategy:</label><br>
+        <input type="radio" name="strategy" value="smart" required> Smart
+        <input type="radio" name="strategy" value="random" required> Random<br>
+        <input type="submit" class="button" value="Submit">
+    </form>
+</html>
+<?php endif;
+$board = new board();
+if (isset($_POST['strategy'])) {
+    $strategy = $_POST['strategy'];
+    echo "You selected the '$strategy' strategy!";
+    if ($strategy == 'smart') {
+        $strategy = new SmartStrategy($board);
+    } elseif ($strategy == 'random') {
+        $strategy = new RandomStrategy($board);
+    }
+}
 
 echo json_encode($gameInfo);
+
+
 
 ?>
